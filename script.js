9d64cfa1070799f239b143b411b0106f4ad2b1df
s235004 <s2350004@matsu.shimane-u.ac.jp>
2025-03-27 20:32:35 +0900

    mainのレスポンシブを実装

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ requestAnimationFrame(raf);
 lenis.on('scroll', (e) => {
     console.log(e);
 });
+
+//スムーズスクロール
+$(function(){
+    $('a[href^=#]').click(function() {
+    var speed = 500; // スクロール速度(ミリ秒)
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+    });
+});
